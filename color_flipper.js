8c62e74ea9e214ae6fcafe382a8f999a78946566
mooky1007 <mooky1007@naver.com>
2023-05-19 13:28:24 +0900

    리팩토링

--- color_flipper.js
+++ color_flipper.js
@@ -1,40 +1,48 @@
 class ColorFlipper {
     constructor(target) {
         this.target = document.querySelector(target);
-        this.colorCode = this.target.querySelector(".color_value");
-        this.actionButton = this.target.querySelector(".color_btn");
+        this.colorCode = this.target.querySelector('.color_value');
+        this.actionButton = this.target.querySelector('.color_btn');
         this.init();
     }
 
     init() {
-        this.actionButton.addEventListener("click", this.changeColor.bind(this));
+        this.actionButton.addEventListener('click', this.changeColor.bind(this));
         this.changeColor();
     }
 
     changeColor() {
+        const randomColor = this.generateRandomColor();
+        this.setRandomColor(randomColor);
+        this.updateTextColor(randomColor);
+    }
+
+    generateRandomColor() {
         const r = Math.floor(Math.random() * 256);
         const g = Math.floor(Math.random() * 256);
         const b = Math.floor(Math.random() * 256);
-        
-        this.checkBackgroundColor(r, g, b)
-
-        const randomColor = `rgb(${r}, ${g}, ${b})`;
-
-        this.target.style.backgroundColor = randomColor;
-        this.colorCode.textContent = `[ ${randomColor} ]`
+        return `rgb(${r}, ${g}, ${b})`;
     }
 
-    checkBackgroundColor(r, g, b) {
-        if (r > 155 && g > 155 && b > 155) {
-            this.colorCode.style.color = "#000";
-            this.actionButton.style.color = "#000";
-            this.actionButton.style.borderColor = "#000";
-            this.target.querySelector("h1").style.color = "#000";
-        }else{
-            this.colorCode.style.color = "#fff";
-            this.actionButton.style.color = "#fff";
-            this.actionButton.style.borderColor = "#fff";
-            this.target.querySelector("h1").style.color = "#fff";
-        }
+    setRandomColor(color) {
+        this.target.style.backgroundColor = color;
+        this.colorCode.textContent = `[ ${color} ]`;
+    }
+
+    updateTextColor(color) {
+        const isLightColor = this.isLightColor(color);
+        const textColor = isLightColor ? '#000' : '#fff';
+        this.colorCode.style.color = textColor;
+        this.actionButton.style.color = textColor;
+        this.actionButton.style.borderColor = textColor;
+        this.target.querySelector('h1').style.color = textColor;
+    }
+
+    isLightColor(color) {
+        const [r, g, b] = color
+            .substring(color.indexOf('(') + 1, color.lastIndexOf(')'))
+            .split(',')
+            .map(Number);
+        return r > 155 && g > 155 && b > 155;
     }
 }
